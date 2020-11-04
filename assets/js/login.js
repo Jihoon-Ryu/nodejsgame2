365782d62603d5a2cdfa93e6c01bc7e8b89c1f72
Jihoon-Ryu <ujihun94@naver.com>
2020-11-04 21:19:39 +0100

    .
    .

--- assets/js/login.js
+++ assets/js/login.js
@@ -10,6 +10,7 @@ const LOGGED_IN = "loggedIn";
 const logIn = (nickname) => {
   //socket접속
   window.socket = io("/");
+  //window를 이용해 socket을 전역변수화
   //그 접속자 본인에게 setNickName이라는 이벤트와 nickname 데이터 전송
   //socketController로 보냄
   window.socket.emit(window.events.setNickname, { nickname });
